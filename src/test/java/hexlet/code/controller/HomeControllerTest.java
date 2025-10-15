0891c12e772d02bf60c29537fe1c610aaea55fc2
Pavel Tretyakov <tretyakovpavel54@mail.ru>
2025-10-15 23:41:07 +0700

    mod

---
+++ src/test/java/hexlet/code/controller/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package hexlet.code.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Добро пожаловать в Hexlet Spring Blog!"));
+    }
+    @Test
+    void testAboutPage() throws Exception {
+        mockMvc.perform(get("/about"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("This is simple Spring blog!"));
+    }
+}
